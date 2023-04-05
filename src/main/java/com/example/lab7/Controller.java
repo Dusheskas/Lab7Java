1cb19555021abac329ef61fa2a5e09b91090ed89
Dusheskas <andrew.salomatinn@gmail.com>
2023-04-05 11:22:11 +0300

    Lab 7 _ 1

---
+++ src/main/java/com/example/lab7/Controller.java
@@ -0,0 +1,41 @@
+package com.example.lab7;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField textField;
+    @FXML
+    private Label labelRes;
+
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void onExitBtn(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    void onSetBtn(ActionEvent event) {
+        labelRes.setText(textField.getText());
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
